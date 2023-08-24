cd5e670b882eee920e0fd079abd303da7b241e9e
tllevydavis <143045150+tllevydavis@users.noreply.github.com>
2023-08-23 20:14:36 -0400

    Add files via upload

---
+++ Notes.py
@@ -0,0 +1,10 @@
+# Data Types
+age=32 # whole numbers are integers    C++ int age=32;
+age=5.5 # decimals number are float C++ float age=5.5;
+name="Toni" # string(squence of characters)
+isValid=True 
+print(age) # C++ //(comment)
+multiline C++ /*  */
+comment
+
+age=32  # assignment statment  LeftHandside(variable)=RightHandSide(expressiom)
